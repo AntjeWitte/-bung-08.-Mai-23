e86241e02d46c89a4032cd170db49298a00a25fa
Antje Witte <antjewitte90@gmail.com>
2023-05-08 11:40:25 +0200

    fertig

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-function toggleButton() {
-  const cards = document.querySelectorAll("[data-js=card]");
-  const buttons = card.querySelectorAll("[data-js=buttons]");
-  cards.forEach(card => {
-        const button = card.querySelector('[data-js="button"]')
-        const link =  cards.querySelector('[data-js="link"]')
-        const arrow = card.querySelector('[data-js="arrow"]')
-          button.addEventListener('click',() => {
-          link.classList.toggle("hide")
-          arrow.classList.toggle("rotate")
-          
-    })
-})
-}
+// function toggleButton() {
+//   const cards = document.querySelectorAll('[data-js="card"]');
+//   const buttons = document.querySelectorAll('[data-js="button"]');
+//   cards.forEach(card => {
+//     const button = card.querySelector('[data-js="button"]');
+//     const link = card.querySelector('[data-js="link"]');
+//     const arrow = card.querySelector('[data-js="arrow"]');
+//     buttons.addEventListener('click', () => {
+//       link.classList.toggle('hide');
+//       arrow.classList.toggle('rotate');
+//     });
+//   });
+// }
+
+// toggleButton();
+
+const cards = document.querySelectorAll('[data-js="card"]');
+//const buttons = cards.querySelectorAll('[data-js="button"]');
+
+//function toggleButton() {
+
+cards.forEach(card => {
+  const button = card.querySelector('[data-js="button"]');
+  const link = card.querySelector('[data-js="link"]');
+  const arrow = button.querySelector('[data-js="arrow"]');
+  button.addEventListener('click', () => {
+    link.classList.toggle('hidden');
+    arrow.classList.toggle('rotate');
+  });
+});
+//}
+
+//toggleButton();
