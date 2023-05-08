fb12c2c9f0966e489064308c83e21bc7a4427aa3
Antje Witte <antjewitte90@gmail.com>
2023-05-08 11:45:05 +0200

    besser

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-// function toggleButton() {
-//   const cards = document.querySelectorAll('[data-js="card"]');
-//   const buttons = document.querySelectorAll('[data-js="button"]');
-//   cards.forEach(card => {
-//     const button = card.querySelector('[data-js="button"]');
-//     const link = card.querySelector('[data-js="link"]');
-//     const arrow = card.querySelector('[data-js="arrow"]');
-//     buttons.addEventListener('click', () => {
-//       link.classList.toggle('hide');
-//       arrow.classList.toggle('rotate');
-//     });
-//   });
-// }
-
-// toggleButton();
-
 const cards = document.querySelectorAll('[data-js="card"]');
-//const buttons = cards.querySelectorAll('[data-js="button"]');
-
-//function toggleButton() {
 
 cards.forEach(card => {
   const button = card.querySelector('[data-js="button"]');
@@ -28,6 +9,3 @@ cards.forEach(card => {
     arrow.classList.toggle('rotate');
   });
 });
-//}
-
-//toggleButton();
